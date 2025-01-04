74108d6c285afc2f807f17a12b176d0b52ccc550
半島奶茶 <11590149+lrj1996@user.noreply.gitee.com>
2025-01-04 18:37:09 +0800

    删除多余代码

--- SnailIOTMonitoring/FrameWork/centerwidget.cpp
+++ SnailIOTMonitoring/FrameWork/centerwidget.cpp
@@ -8,11 +8,6 @@ centerWidget::centerWidget(QWidget *parent) :
     ui->setupUi(this);
 
     m_QStackedLayout = new QStackedLayout(this);
-    //m_TCPClient = new TCPClient(this);
-
-    //m_QStackedLayout->addWidget(m_TCPClient);
-
-    //connect(m_TCPClient,&SerialPort::sendMessageBySerial,this,&centerWidget::sendMessageByCenter);
 }
 
 centerWidget::~centerWidget()
