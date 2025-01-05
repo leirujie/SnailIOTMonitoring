6affaf06fcda17932b6cbb1651a8779bbd64b62c
雷汝杰 <103736884+leirujie@users.noreply.github.com>
2025-01-05 16:16:18 +0800

    更新页面索引枚举值

--- SnailIOTMonitoring/FrameWork/centerwidget.h
+++ SnailIOTMonitoring/FrameWork/centerwidget.h
@@ -13,9 +13,20 @@ class RegisterPage;
 
 // 定义页面索引枚举
 enum PageIndex {
-    LAUNCH_SCREEN_PAGE = 0,//启动页面
-    ACCOUNT_PWD_LOGIN_PAGE = 1,//登陆界面
-    REGISTER_PAGE = 2  // 注册页面
+    LAUNCH_SCREEN_PAGE = 0, // 启动页面
+    ACCOUNT_PWD_LOGIN_PAGE = 1, // 登录界面
+    REGISTER_PAGE = 2, // 注册页面
+    DEVICE_ADD_PAGE = 3, // 设备添加页面
+    DEVICE_DEL_PAGE = 4, // 设备删除页面
+    DEVICE_EDIT_PAGE = 5, // 设备编辑页面
+    DEVICE_GROUP_PAGE = 6, // 设备分组页面
+    DEVICE_ADD_GROUP_PAGE = 7, // 设备添加分组页面
+    DEVICE_DEL_GROUP_PAGE = 8, // 设备删除分组页面
+    DEVICE_EDIT_GROUP_PAGE = 9, // 设备编辑分组页面
+    DEVICE_DATA_PAGE = 10, // 设备数据页面
+    DEVICE_DATA_HISTORY_PAGE = 11, // 设备数据历史页面
+    DEVICE_ALARM_PAGE = 12, // 设备告警页面
+    LOG_PAGE = 13, // 日志页面
 };
 
 namespace Ui {
