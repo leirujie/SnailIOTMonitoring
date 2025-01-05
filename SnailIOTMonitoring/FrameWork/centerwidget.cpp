e71130cab0073e37f7d4791721a21c57ad3b2af8
雷汝杰 <1819131456@qq.com>
2025-01-05 15:51:43 +0800

    更新centerwidget模块

--- SnailIOTMonitoring/FrameWork/centerwidget.cpp
+++ SnailIOTMonitoring/FrameWork/centerwidget.cpp
@@ -6,9 +6,42 @@ centerWidget::centerWidget(QWidget *parent) :
     ui(new Ui::centerWidget)
 {
     ui->setupUi(this);
-
+    // 创建各个页面指针指向的对象
+    launchscrren = new LaunchScrren;
+    loginPage = new LoginPage;
+    registerPage = new RegisterPage;
     m_QStackedLayout = new QStackedLayout(this);
+
+    //添加页面
+    m_QStackedLayout->addWidget(launchscrren);//启动页面
+    m_QStackedLayout->addWidget(loginPage);
+    m_QStackedLayout->addWidget(registerPage);
+    m_QStackedLayout->setCurrentWidget(launchscrren);
+
+    // 页面切换
+    connect(launchscrren, &LaunchScrren::switchPage, this, &centerWidget::onSwitchPage);
+    connect(loginPage, &LoginPage::switchPage, this, &centerWidget::onSwitchPage);
+    connect(registerPage, &RegisterPage::switchPage, this, &centerWidget::onSwitchPage);
+    connect(launchscrren,&LaunchScrren::sendMsg,this,&centerWidget::sendMessageByCenter);
 }
+
+void centerWidget::onSwitchPage(int targetPageIndex)
+{
+    switch (targetPageIndex) {
+    case LAUNCH_SCREEN_PAGE:
+        m_QStackedLayout->setCurrentWidget(launchscrren);
+        break;
+    case ACCOUNT_PWD_LOGIN_PAGE:
+        m_QStackedLayout->setCurrentWidget(loginPage);
+        break;
+    case REGISTER_PAGE:
+        m_QStackedLayout->setCurrentWidget(registerPage);
+        break;
+    default:
+        break;
+    }
+}
+
 
 centerWidget::~centerWidget()
 {
--- SnailIOTMonitoring/FrameWork/centerwidget.h
+++ SnailIOTMonitoring/FrameWork/centerwidget.h
@@ -3,6 +3,20 @@
 
 #include <QWidget>
 #include <QStackedLayout>
+#include "userManager/launchscrren.h"
+#include "userManager/loginpage.h"
+#include "userManager/registerpage.h"
+
+class LaunchScrren;
+class LoginPage;
+class RegisterPage;
+
+// 定义页面索引枚举
+enum PageIndex {
+    LAUNCH_SCREEN_PAGE = 0,//启动页面
+    ACCOUNT_PWD_LOGIN_PAGE = 1,//登陆界面
+    REGISTER_PAGE = 2  // 注册页面
+};
 
 namespace Ui {
 class centerWidget;
@@ -18,11 +32,16 @@ public:
 
 signals:
     void sendMessageByCenter(QString);
-public slots:
+
+private slots:
+    void onSwitchPage(int targetPageIndex);  // 用于接收切换页面的信号
 
 private:
     Ui::centerWidget *ui;
     QStackedLayout *m_QStackedLayout;
+    LaunchScrren *launchscrren;//启动页
+    LoginPage *loginPage;      //登录页
+    RegisterPage *registerPage;//注册页
 };
 
 #endif // CENTERWIDGET_H
